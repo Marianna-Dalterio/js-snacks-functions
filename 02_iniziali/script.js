6561df8e84b37f6fd3f09d6566a62a74a63e052e
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-19 09:33:26 +0200

    02. add function and arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+// function nameFirstLetter(names) {
+//     firstLetter = [];
+//     for (let i = 0; i < names.length; i++) {
+//         firstLetter.push(names[i][0]);
+//     }
+//     return firstLetter
 
+// }
+
+//ARROW FUNCTION 
+const nameFirstLetter = (names) => {
+    firstLetter = [];
+    for (let i = 0; i < names.length; i++) {
+        firstLetter.push(names[i][0]);
+    }
+    return firstLetter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(nameFirstLetter(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
