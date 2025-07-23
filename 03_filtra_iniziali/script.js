7711b4666d2b45f2387aaa45f20e5e41c2386f82
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-23 10:39:21 +0200

    03 function

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+//FUNZIONE
 
+function namesWithA(names, letter) {
+    const result = [];
+    for (i = 0; i < names.length; i++) {
+        const thisName = names[i]
+        console.log(names[i])
+        if (thisName.startsWith("A")) {
+            result.push(thisName)
+        }
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const filteredNames = namesWithA(names, "A");
+console.log(filteredNames)
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
