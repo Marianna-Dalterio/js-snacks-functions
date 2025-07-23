b2841894bb58c90e5ca43552e9d672e216877b2a
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-23 11:08:37 +0200

    04. function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(word) {
+    const count = word.match(/[aeiou]/gi)
+    if (count === null) {
+        console.log("Nessuna vocale è stata trovata")
+        return 0
+    } else {
+        console.log("Vocali trovate:", count)-- > //stampa le vocali oltre al numero 
+        return count.length
+    }
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = countVowels(word);
+console.log(result)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
