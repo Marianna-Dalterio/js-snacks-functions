3f978435a5c0ac1add7452d650603cc1d8e38aa9
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-07-23 12:05:10 +0200

    05. function

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,24 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function greeting(name) {
+    const hour = new Date().getHours();
+    let greetingText;
+    if (hour <= 13) {
+        greetingText = (`Buongiorno ${name}`)
+    } else if (hour <= 17) {
+        greetingText = (`Buon pomeriggio ${name}`)
+
+    } else {
+        greetingText = (`Buonasera ${name}`)
+    }
+    return greetingText
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = greeting("Mario")
+console.log(result)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
